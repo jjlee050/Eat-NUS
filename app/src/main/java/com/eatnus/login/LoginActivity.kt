fb9815b400e671648b7c03355b9a4772bf8bdea2
joseph <josephlee050@gmail.com>
2018-06-04 10:22:20 +0800

    Initial Project

---
+++ app/src/main/java/com/eatnus/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.eatnus.ui.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.eatnus.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
